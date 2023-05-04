2778fc2676bbf97651f6ea48c8416fde3ca90db1
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-05-05 01:24:32 +0300

    ERR fix

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,30 +1,6 @@
--- Test view
-SELECT * FROM need_meeting;
-
-SELECT "--";
-
-UPDATE students SET score = 40 WHERE name = 'Bob';
-SELECT * FROM need_meeting;
-
-SELECT "--";
-
-UPDATE students SET score = 80 WHERE name = 'Steeve';
-SELECT * FROM need_meeting;
-
-SELECT "--";
-
-UPDATE students SET last_meeting = CURDATE() WHERE name = 'Jean';
-SELECT * FROM need_meeting;
-
-SELECT "--";
-
-UPDATE students SET last_meeting = ADDDATE(CURDATE(), INTERVAL -2 MONTH) WHERE name = 'Jean';
-SELECT * FROM need_meeting;
-
-SELECT "--";
-
-SHOW CREATE TABLE need_meeting;
-
-SELECT "--";
-
-SHOW CREATE TABLE students;
+-- A SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
