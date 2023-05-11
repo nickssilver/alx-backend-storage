29fcdc526ec9eca3d949208bee336b57d8cfdef7
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-05-11 11:26:47 +0300

    :hammer:

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,58 +1,24 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
+import redis
 import requests
-import time
-from functools import wraps
+from datetime import timedelta
+
 
 def get_page(url: str) -> str:
-    cache_key = f"count:{url}"
-    cached_result = cache.get(cache_key)
-    if cached_result is not None:
-        return cached_result
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
-    cache.set(cache_key, content, expire=10)
-    return content
-
-class Cache:
-    def __init__(self):
-        self.cache = {}
-
-    def get(self, key):
-        if key not in self.cache:
-            return None
-        value, expiration_time = self.cache[key]
-        if expiration_time is not None and time.time() > expiration_time:
-            del self.cache[key]
-            return None
-        return value
-
-    def set(self, key, value, expire=None):
-        if expire is not None:
-            expiration_time = time.time() + expire
-        else:
-            expiration_time = None
-        self.cache[key] = (value, expiration_time)
-
-cache = Cache()
-
-def cached(expiration_time=10):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            cache_key = f"count:{args[0]}"
-            cached_result = cache.get(cache_key)
-            if cached_result is not None:
-                return cached_result
-            result = func(*args, **kwargs)
-            cache.set(cache_key, result, expire=expiration_time)
-            return result
-        return wrapper
-    return decorator
-
-@cached(expiration_time=10)
-def get_page_cached(url: str) -> str:
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
-    return content
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
+        redis_store.incr(req_key)
+        return result
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
